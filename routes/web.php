ef8f0eb1dc825abb13122cd171b191fed6aa4e46
Nasser Al-Abdullah <nasser-alabdullah@outlook.com>
2024-06-29 14:29:43 +0300

    Issue with success message

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.store'); // ModelName.ActionTaken
+
+Route::delete('/ideas/{id}', [IdeaController::class, 'store'])->name('ideas.destroy'); // ModelName.ActionTaken
 
 Route::get('/terms', function() {
     return view('terms');
